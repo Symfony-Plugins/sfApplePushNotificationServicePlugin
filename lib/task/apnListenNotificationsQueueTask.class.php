916cf3bf645c70aec966e68e012230b9829ef408
NeX <NeX@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-12-15 12:08:50 -0600

    Adding debug mode to listen.er task
    git-svn-id: http://svn.symfony-project.com/plugins/sfApplePushNotificationServicePlugin/trunk@25414 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/apnListenNotificationsQueueTask.class.php
+++ lib/task/apnListenNotificationsQueueTask.class.php
@@ -4,26 +4,41 @@ class apnListenApnQueueTask extends sfBaseTask {
     	$this->namespace = 'apn';
         $this->name = 'listen-apn-queue';
         $this->briefDescription = 'Starts a listener that will take every notification message from the apn_queue and send them to the users through the Apple Push Notification Service';
+        
+        $this->addOptions(
+            array(
+                new sfCommandOption('debug_mode', null, sfCommandOption::PARAMETER_OPTIONAL, 'Enable debug mode [on|off]'),
+                new sfCommandOption('token', null, sfCommandOption::PARAMETER_OPTIONAL, 'Debug apns token'),
+                new sfCommandOption('message', null, sfCommandOption::PARAMETER_OPTIONAL, 'Debug message'),
+            )
+        );
     }
   	
     protected function execute($arguments = array(), $options = array()) {
     	$arguments['config'] = sfYaml::load(sfConfig::get('sf_root_dir')."/plugins/sfApplePushNotificationServicePlugin/config/config.yml");
-    	while(true) {
-            $pid = pcntl_fork();
-            if($pid == -1){
-                die('could not fork');
-            } else if ($pid) {
-                pcntl_wait($status);
-                pcntl_waitpid($pid, $stat);
-            } else {
-                try{
-                    $this->executePartial($arguments, $options);
-                        posix_kill(getmypid(),9);
-                } catch(Exception $e) {
-                        posix_kill(getmypid(),9);
-                }
-            }
-	    }
+    	if(isset($options['debug_mode']) && $options['debug_mode'] == 'on') {
+            $apn_connection = new ApnConecction();
+            $apn_connection->connect();
+            $apn_connection->send($options['token'], $options['message']);
+            $apn_connection->disconnect();
+    	} else {            
+	    	while(true) {
+	            $pid = pcntl_fork();
+	            if($pid == -1){
+	                die('could not fork');
+	            } else if ($pid) {
+	                pcntl_wait($status);
+	                pcntl_waitpid($pid, $stat);
+	            } else {
+	                try{
+	                    $this->executePartial($arguments, $options);
+	                        posix_kill(getmypid(),9);
+	                } catch(Exception $e) {
+	                        posix_kill(getmypid(),9);
+	                }
+	            }
+	        }
+    	}
     }
     
     protected function executePartial($arguments = array(), $options = array()) {
